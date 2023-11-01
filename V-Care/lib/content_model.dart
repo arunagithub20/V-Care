ef1902cc3c2254e071ab1ad7d3e8b8cb1c2d091e
Aruna Priyadarshana <apriyadarshana41@gmail.com>
2023-11-01 14:18:45 +0530

    Updating Onboarding Conent

--- V-Care/lib/content_model.dart
+++ V-Care/lib/content_model.dart
@@ -10,36 +10,26 @@ List<UnbordingContent> contents = [
   UnbordingContent(
     title: 'Vehicle Maintenance',
     image: 'images/maintenance',
-    discription: "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the "
-    "industry's standard dummy text ever since the 1500s, "
-    "when an unknown printer took a galley of type and scrambled it "
+    discription: "Regular vehicle maintenance is the key to keeping your car running smoothly and ensuring your safety on the road. Stay on top of maintenance tasks, receive reminders, and access expert tips to extend the life of your vehicle. With our app, you can keep your car in top condition and enjoy worry-free driving."
   ),
   UnbordingContent(
     title: 'Spare Parts',
     image: 'images/spareparts',
-    discription: "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the "
-    "industry's standard dummy text ever since the 1500s, "
-    "when an unknown printer took a galley of type and scrambled it "
+    discription: "Find the right spare parts for your vehicle with ease. Our app offers a vast selection of high-quality parts, from batteries to brakes. Quickly locate what you need, place orders, and get them delivered to your doorstep. Keep your vehicle running smoothly with our convenient spare parts solution."
   ),
   UnbordingContent(
     title: 'Buying & Selling',
     image: 'images/buysell',
-    discription: "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the "
-    "industry's standard dummy text ever since the 1500s, "
-    "when an unknown printer took a galley of type and scrambled it "
+    discription: "Discover a world of possibilities in buying and selling. Our app connects buyers and sellers, making it effortless to find what you're looking for or sell items you no longer need. Enjoy secure transactions, explore a wide range of products, and turn your unwanted items into cash. Elevate your buying and selling experience with our user-friendly app."
   ),
   UnbordingContent(
     title: 'Breakdown Services',
     image: 'images/breakdown',
-    discription: "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the "
-    "industry's standard dummy text ever since the 1500s, "
-    "when an unknown printer took a galley of type and scrambled it "
+    discription: "Stuck on the road? Our app is your lifeline. Access immediate nearest garage with just a tap. We connect you with trusted service providers, offering fast and reliable help. Whether it's a flat tire, engine trouble, or a dead battery, count on us to get you back on your way. Your peace of mind is just a click away."
   ),
   UnbordingContent(
     title: 'Location Mapping',
     image: 'images/location',
-    discription: "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the "
-    "industry's standard dummy text ever since the 1500s, "
-    "when an unknown printer took a galley of type and scrambled it "
+    discription: "Unexpected car trouble in an unfamiliar place? No worries. Our app has you covered. With our location services, we'll pinpoint your exact position and connect you with nearby assistance, ensuring you're never stranded. "
   ),
 ];
