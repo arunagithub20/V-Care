969c383833bed47c42f78e705af7932b1f27b855
Aruna Priyadarshana <apriyadarshana41@gmail.com>
2023-11-01 10:19:09 +0530

    Add content_model.dart

---
+++ lib/content_model.dart
@@ -0,0 +1,45 @@
+class UnbordingContent {
+  String image;
+  String title;
+  String discription;
+
+  UnbordingContent({required this.image, required this.title, required this.discription});
+}
+
+List<UnbordingContent> contents = [
+  UnbordingContent(
+    title: 'Vehicle Maintenance',
+    image: 'images/maintenance',
+    discription: "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the "
+    "industry's standard dummy text ever since the 1500s, "
+    "when an unknown printer took a galley of type and scrambled it "
+  ),
+  UnbordingContent(
+    title: 'Spare Parts',
+    image: 'images/spareparts',
+    discription: "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the "
+    "industry's standard dummy text ever since the 1500s, "
+    "when an unknown printer took a galley of type and scrambled it "
+  ),
+  UnbordingContent(
+    title: 'Buying & Selling',
+    image: 'images/buysell',
+    discription: "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the "
+    "industry's standard dummy text ever since the 1500s, "
+    "when an unknown printer took a galley of type and scrambled it "
+  ),
+  UnbordingContent(
+    title: 'Breakdown Services',
+    image: 'images/breakdown',
+    discription: "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the "
+    "industry's standard dummy text ever since the 1500s, "
+    "when an unknown printer took a galley of type and scrambled it "
+  ),
+  UnbordingContent(
+    title: 'Location Mapping',
+    image: 'images/location',
+    discription: "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the "
+    "industry's standard dummy text ever since the 1500s, "
+    "when an unknown printer took a galley of type and scrambled it "
+  ),
+];
