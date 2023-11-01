45af566b3bfaf48f88b692c56d09ebf345b94c4e
Aruna Priyadarshana <apriyadarshana41@gmail.com>
2023-11-01 10:30:26 +0530

    Add getstart UI of V-Care

---
+++ lib/pages/getstart.dart
@@ -0,0 +1,130 @@
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+
+import '../content_model.dart';
+
+
+class Onbording extends StatefulWidget {
+  @override
+  _OnbordingState createState() => _OnbordingState();
+}
+
+class _OnbordingState extends State<Onbording> {
+  int currentIndex = 0;
+  late PageController _controller;
+  
+
+  @override
+  void initState() {
+    _controller = PageController(initialPage: 0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              child: PageView.builder(
+                controller: _controller,
+                itemCount: contents.length,
+                onPageChanged: (int index) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                },
+                itemBuilder: (_, i) {
+                  return Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          contents[i].image,
+                          height: 300,
+                        ),
+                        Text(
+                          contents[i].title,
+                          style: const TextStyle(
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          contents[i].discription,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List.generate(
+                    contents.length,
+                    (index) => buildDot(index, context),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10,left: 20, right: 20),
+              child: InkWell(
+                            onTap: () {
+                           
+                            },
+                            child: Container(
+                              height: 50,
+                              decoration: BoxDecoration(
+                                color: const Color.fromARGB(255, 1, 43, 157),
+                                borderRadius: BorderRadius.circular(35),
+                              ),
+                              child: const Center(
+                                child: Text("Get Start",style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                ),
+                              ),
+                            ),
+                          ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container buildDot(int index, BuildContext context) {
+    return Container(
+      height: 10,
+      width: currentIndex == index ? 25 : 10,
+      margin: EdgeInsets.only(right: 5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Color.fromARGB(255, 4, 64, 230),
+      ),
+    );
+  }
+}
