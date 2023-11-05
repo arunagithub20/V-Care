5c4ca73f25f8ef46a3f485f0a5f813cfbfe46393
Aruna Priyadarshana <apriyadarshana41@gmail.com>
2023-11-05 23:17:51 +0530

    Routing Interfaces
    Routing usertype  & spareparts shop owner interface

--- V-Care/lib/pages/usertype.dart
+++ V-Care/lib/pages/usertype.dart
@@ -1,117 +1,124 @@
 import 'package:flutter/material.dart';
+import 'package:getstart/pages/sparepartsshop.dart';
 
 class allaccount extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/background"),
-              fit: BoxFit.cover,
+      home: SafeArea(
+        child: Scaffold(
+          body: Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/background"),
+                fit: BoxFit.cover,
+              ),
+      
             ),
-
-          ),
-
-          child: Center(
-                child: Container(
-                  height: double.infinity,
-                  width: double.infinity,
-                  margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 50.0, bottom: 40.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.7),
-                    borderRadius: BorderRadius.circular(15),
-                  ),
+      
+            child: Center(
                   child: Container(
-                    padding: EdgeInsets.all(20.0),
-                    child: Form(
-                      
-                      child: ListView(
-                        children: [
-                          DefaultTextStyle.merge(
-                          style: const TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          child: const Center(
-                            child: Text('Choose Your User Type'),
-                          ),
-                        ),
+                    height: double.infinity,
+                    width: double.infinity,
+                    margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 50.0, bottom: 40.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.7),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Container(
+                      padding: EdgeInsets.all(20.0),
+                      child: Form(
                         
-                          const SizedBox(height: 40),
-                          Container(
-                            height: 66.0,
-                            child: ElevatedButton(
-                              onPressed: () =>{},
-                              child: Text('Vehicle Owner'),
-                              style: ElevatedButton.styleFrom(
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(55),
+                        child: ListView(
+                          children: [
+                            DefaultTextStyle.merge(
+                            style: const TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            child: const Center(
+                              child: Text('Choose Your User Type'),
+                            ),
+                          ),
+                          
+                            const SizedBox(height: 40),
+                            Container(
+                              height: 66.0,
+                              child: ElevatedButton(
+                                onPressed: () =>{},
+                                child: Text('Vehicle Owner'),
+                                style: ElevatedButton.styleFrom(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(55),
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-
-                          const SizedBox(height: 50),
-
-                          Container(
-                            height: 66.0,
-                            child: ElevatedButton(
-                              onPressed: () =>{},
-                              child: Text('Shop Owner'),
-                              style: ElevatedButton.styleFrom(
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(55),
+      
+                            const SizedBox(height: 50),
+      
+                            Container(
+                              height: 66.0,
+                              child: ElevatedButton(
+                                onPressed: () =>{
+                                  Navigator.of(context).push(MaterialPageRoute(builder: (context){
+                                    return sparepartshop();
+                                  }))
+                                },
+                                child: Text('Shop Owner'),
+                                style: ElevatedButton.styleFrom(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(55),
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-
-                          const SizedBox(height: 50),
-
-                          Container(
-                            height: 66.0,
-                            child: ElevatedButton(
-                              onPressed: () =>{},
-                              child: Text('Garage Owner'),
-                              style: ElevatedButton.styleFrom(
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(55),
+      
+                            const SizedBox(height: 50),
+      
+                            Container(
+                              height: 66.0,
+                              child: ElevatedButton(
+                                onPressed: () =>{},
+                                child: Text('Garage Owner'),
+                                style: ElevatedButton.styleFrom(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(55),
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-
-                          const SizedBox(height: 50),
-
-                          Container(
-                            height: 66.0,
-                            child: ElevatedButton(
-                              onPressed: () =>{},
-                              child: Text('Buyers & Sellers'),
-                              style: ElevatedButton.styleFrom(
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(55),
+      
+                            const SizedBox(height: 50),
+      
+                            Container(
+                              height: 66.0,
+                              child: ElevatedButton(
+                                onPressed: () =>{},
+                                child: Text('Buyers & Sellers'),
+                                style: ElevatedButton.styleFrom(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(55),
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-
-
-                        ],
-                      ),
-                      ),
+      
+      
+                          ],
+                        ),
+                        ),
+                    ),
                   ),
                 ),
-              ),
-
-
-        )
-
+      
+      
+          )
+      
+        ),
       ),
 
     );
