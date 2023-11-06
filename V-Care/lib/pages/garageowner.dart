9bf018056b2b3f6be6802113adb5baca63351706
Ishara Hewage <ishararuchiranga.aht@gmail.com>
2023-11-06 21:47:53 +0530

    routing_vehicleowner,buyer&seller_sign-up_interfaces

--- V-Care/lib/pages/garageowner.dart
+++ V-Care/lib/pages/garageowner.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
 
-
 class garageowner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Container(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+            body: Container(
           width: double.infinity,
           height: double.infinity,
           decoration: BoxDecoration(
@@ -15,115 +16,98 @@ class garageowner extends StatelessWidget {
               image: AssetImage("assets/background"),
               fit: BoxFit.cover,
             ),
-
           ),
-
           child: Center(
-                child: Container(
-                  height: double.infinity,
-                  width: double.infinity,
-                  margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 40.0, bottom: 40.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.7),
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: Container(
-                    padding: EdgeInsets.all(20.0),
-                    child: Form(
-                      
-                      child: ListView(
-                        children: [
-                          TextFormField(
-                            decoration: InputDecoration(
-                              labelText: 'Business Name',
-                              border: OutlineInputBorder(borderRadius: BorderRadius.circular(35)),
-                            ),
-                          ),
-                        
-                          const SizedBox(height: 30),
-
-                          TextFormField(
-                            decoration: InputDecoration(
-                              labelText: 'Business Registration Number',
-                              border: OutlineInputBorder(borderRadius: BorderRadius.circular(35)),
-                            ),
-                          ),
-                        
-                          const SizedBox(height: 30),
-
-                          TextFormField(
-                            decoration: InputDecoration(
-                              labelText: 'Location',
-                              border: OutlineInputBorder(borderRadius: BorderRadius.circular(35)),
-                            ),
-                          ),
-                        
-                          const SizedBox(height: 30),
-
-                          IntlPhoneField(
-                            decoration: InputDecoration(
-                              labelText: 'Phone Number',
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(35),
-                                borderSide: BorderSide(),
-                              ),
-                            ),
-                            initialCountryCode: 'LK',
-                            onChanged: (phone) {
-                              print(phone.completeNumber);
-                            },
-                          ),
-
-                          const SizedBox(height: 50),
-
-                          TextFormField(
-                              obscureText: true,
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(borderRadius: BorderRadius.circular(35)),
-                                labelText: 'Password',
-                              ),
-                             ),
-                          const SizedBox(height: 20),
-                          TextFormField(
-                              
-                              obscureText: true,
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(borderRadius: BorderRadius.circular(35)),
-                                labelText: 'Confirm Password',
-                              ),
-                              ),
-                              
-                          const SizedBox(height: 40),
-
-
-                          Container(
-                            height: 66.0,
-                            child: ElevatedButton(
-                              onPressed: () =>{},
-                              child: Text('Sign Up'),
-                              style: ElevatedButton.styleFrom(
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(55),
-                                ),
-                              ),
-                            ),
-                          ),
-
-
-                        ],
+            child: Container(
+              height: double.infinity,
+              width: double.infinity,
+              margin: EdgeInsets.only(
+                  left: 20.0, right: 20.0, top: 40.0, bottom: 40.0),
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.7),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Container(
+                padding: EdgeInsets.all(20.0),
+                child: Form(
+                  child: ListView(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Business Name',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(35)),
+                        ),
                       ),
-                    ),
+                      const SizedBox(height: 30),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Business Registration Number',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(35)),
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Location',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(35)),
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      IntlPhoneField(
+                        decoration: InputDecoration(
+                          labelText: 'Phone Number',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(35),
+                            borderSide: BorderSide(),
+                          ),
+                        ),
+                        initialCountryCode: 'LK',
+                        onChanged: (phone) {
+                          print(phone.completeNumber);
+                        },
+                      ),
+                      const SizedBox(height: 50),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(35)),
+                          labelText: 'Password',
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(35)),
+                          labelText: 'Confirm Password',
+                        ),
+                      ),
+                      const SizedBox(height: 40),
+                      Container(
+                        height: 66.0,
+                        child: ElevatedButton(
+                          onPressed: () => {},
+                          child: Text('Sign Up'),
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(55),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
-
-
-        )
-
+            ),
+          ),
+        )),
       ),
-
     );
-    
   }
-
 }
